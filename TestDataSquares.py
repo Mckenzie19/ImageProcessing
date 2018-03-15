91ed5e9cb05371c1af99a7e384e42e74b9f5f96a
Mckenzie Ebert <ebert097@umn.edu>
2018-03-15 15:33:00 -0500

    Fixed makeTestData ranges

--- TestDataSquares.py
+++ TestDataSquares.py
@@ -9,17 +9,19 @@ def makeTestData(numSquares):
     for s in range(len(squares)):
         squares[s] = np.zeros((500,500))
         for i in range(s+100, s+150):
-            squares[s][i][s+10] = 1
-            squares[s][i][s+11] = 1
-            squares[s][i][s+12] = 1
-            squares[s][i][s+58] = 1
-            squares[s][i][s+59] = 1
-            squares[s][i][s+60] = 1
+            squares[s][i][s+100] = 1
+            squares[s][i][s+101] = 1
+            squares[s][i][s+102] = 1
 
-        for j in range(s+10, s+60):
-            squares[s][s+150][j] = 1
+            squares[s][i][s+147] = 1
+            squares[s][i][s+148] = 1
+            squares[s][i][s+149] = 1
+
+        for j in range(s+100, s+150):
             squares[s][s+149][j] = 1
             squares[s][s+148][j] = 1
+            squares[s][s+147][j] = 1
+
             squares[s][s+102][j] = 1
             squares[s][s+101][j] = 1
             squares[s][s+100][j] = 1
