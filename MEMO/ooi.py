63a1536ecc198d5f79ab0ef1053a6a0b3054eaab
Mckenzie Ebert <ebert097@umn.edu>
2018-03-25 14:34:13 -0500

    Updated code documentation

--- MEMO/ooi.py
+++ MEMO/ooi.py
@@ -1,27 +1,49 @@
 import math
 import itertools
 
+"""
+Contains the class Object of Interest, or OOI. This class defines a generalized way to store and analyze images.
+"""
 
-class OOI(object): #ObjectOfInterest
+
+class OOI(object):
+     """
+     Object of Interest (OOI)
+     Description: Parent class to image analysis classes. Holds ways to analyze basic patterns.
+
+     Functions
+     *alignPatterns: Given two image patterns, returns the best permutation (and the match ratio) of the first pattern which results in the best "match" between the two patterns
+     """
 
      def __init__(self, identifier = None, pattern = None, parents = None, weight = 0, children = None):
+          """
+          Description: Creates an OOI object
+
+          Parameters
+          *identifier: String which will serve as the objects id
+          *pattern: Array of elements which defines the attributes of an image
+          *parents: Dictionary whose keys are the identifiers of the parent objects and whose values are the parent objects
+          *weight: Represents the number of times the pattern has been reinforced, starting with 1 at the pattern's creation
+          *children: Dictionary whose keys are the identifiers of the child objects and whose values are the child objects
+     
+          """
           self.identifier = identifier
           self.weight = weight
-          self.parents = parents #Parents and Children should follow the form: {identifier: object}
+          self.parents = parents
           self.children = children
           self.pattern = pattern
 
 
-     def getPattern(self):
-          return self.pattern
-
-
-
-     #Takes in two patterns, and returns the permuatation of the first pattern that results in the most agreement between the elements
-     #Patterns given to function with format: [number of parts, angleSet1, angleSet2, ..., angleSetN], where angleSetN is a list of angles pertaining to a specific part
-     #Currently assumes that each angle set has the same number of parts - FIX THIS
-     #Assumes each element is either a list or a float
      def alignPatterns(self, patt1, patt2):
+          """
+          Description: Given two image patterns, returns the best match ratio and the permutation of the first pattern that results in this match ratio. This is to account
+          for objects which were analyzed in different orientations, resulting in their patterns having slightly different orders.
+
+          Parameters
+          *patt1: First pattern to be analyzed
+          *patt2: Second pattern to be analyzed
+          """
+          
           bestRatio = float('inf')
           bestPerm = None
           #Since number of parts will currently be assumed to be the same, only need to check permutations of one of the angleSets
@@ -38,7 +60,7 @@ class OOI(object): #ObjectOfInterest
                          elif as1[j] != 0:
                               setMatch += abs(as2[j] - as1[j])/abs(as1[j])
                          else:
-                              setMatch += abs(as2[j] - 0.000001)/0.000001 #Check to make sure this is an okay approximation
+                              setMatch += abs(as2[j] - 0.000001)/0.000001 #Check to make sure this approximation returns accurate results
                               
                     setRatio = setMatch / weight
                     totalMatch += setRatio
